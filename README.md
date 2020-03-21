9c432561d6a9923145207dae0e4eca29f468466e
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-03-21 16:04:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### LEARN JAVA
+
+- Run `Hello World` App
+- Using System.out.println() function for print out the output results
+- Data types & variables:
+  - string
+  - integer
+  - boolean
+  - char
+  - double
+  - float
---
+++ variables_data_types/Main.java
@@ -0,0 +1,22 @@
+package variables_data_types;
+
+public class Main {
+    public static void main(String[] args) {
+        // df integer number
+        int age = 5;
+
+        // df float number
+        double PI = 3.44;
+
+        // df string
+        String name = "youssef";
+
+        // df char
+        char o = 'o';
+
+        // df boolean
+        boolean open = false;
+
+        System.out.println("My name is : " + PI);
+    }
+}
