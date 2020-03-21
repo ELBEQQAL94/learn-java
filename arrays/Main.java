9d11b43e4874aa7e4aaa05f6eaa637febd6a8ccd
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-03-21 19:01:25 +0100

    fisr lesson

---
+++ arrays/Main.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("_______Array_________*");
+
+        // declare array of 5 element
+        int[] arr = new int[5];
+
+    }
+}
